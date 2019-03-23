baa339dc3dbf933771fb94d2e1971fd30a88483f
Ghifari Aditya <ghifari160@ghifari160.com>
2019-03-22 22:57:11 -0500

    Add copyright declaration
    As we are a school sponsored team, this project is partly owned by the
    school and the district.
    
    - Add copyright declaration to main/main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,19 @@
+// Copyright (c) 2019 Maine Township High School West Autonomous
+// Vehicle Competiton Team, Sebastian Moruszewicz, GHIFARI160 and
+// associates, Danica Roque, Veronica Kleinschmidt, Daniel Jeong,
+// and Samuel "Fish" Hooson. All rights reserved.
+//
+// Autonomous Vehicle Competition Team
+// Team Lead: Sebastian Moruszewicz
+// Secretary: Danica Roque
+// Presenter: Samuel "Fish" Hooson
+// Members:   Ghifari Aditya, Daniel Jeong, Veronica Kleinschmidt
+//
+// OID
+// ASN.1:   {iso(1) identified-organization(3) dod(6) internet(1) private(4) enterprise(1) 53559 avc(5) rover(1) bob(1)}
+// dot:     1.3.6.1.4.1.53559.5.1.1
+// OID-IRI: /ISO/Identified-Organization/6/1/4/1/53559/5/1/1
+
 #include <QTRSensors.h>
 #include <Arduino.h>
 #include <wiring.h>
