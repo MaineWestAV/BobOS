a7dc1a1ada632225675308daa7e5993badc2e298
Sebastian Moruszewicz <sebasmorus@gmail.com>
2019-04-13 05:15:34 -0500

    Revisions - Night of Competition
    Implemented the new motor control protocol and found constants to match motor speeds.

--- Archive/main_Deprecated.ino
+++ Archive/main_Deprecated.ino
@@ -5,50 +5,38 @@
 
 #include <QTRSensors.h>
 #include <Wire.h>
+#include <Adafruit_MotorShield.h>
 
-#define NUM_SENSORS              5        // number of sensors used
+#define NUM_SENSORS              6        // number of sensors used
 #define NUM_SAMPLES_PER_SENSOR   4        // average 4 analog samples per sensor reading
 #define EMITTER_PIN              2        // emitter is controlled by digital pin 2
 
-#define MOTOR_SPEED 240            // initial motor speed to un-stall
-#define MOTOR_SPEED_COMP_LEFT 15   // motor characteristic speed compensation
-#define MOTOR_PIN_LEFT_SPEED A4    // pin to set left motor speed
-#define MOTOR_PIN_LEFT_FORWARD A0  // pin to drive left motor forward
-#define MOTOR_PIN_LEFT_REVERSE A1  // pin to drive left motor backward
-#define MOTOR_PIN_RIGHT_SPEED A5   // pin to set right motor speed
-#define MOTOR_PIN_RIGHT_FORWARD A2 // pin to drive right motor forward
-#define MOTOR_PIN_RIGHT_REVERSE A3 // pin to drive right motor backward
+#define MOTOR_DURATION           1        // duration of each motor "step" (in secs)
+#define MOTOR_SPEED              150      // speed of each motor
+#define MOTOR_PIN_LEFT           1        // pin location of the left motor
+#define MOTOR_PIN_RIGHT          3        // pin location of the right motor
 
-#define VISIBLE_THRESHOLD -1000    // sensor value threshold for tape visibility
-#define LEFT_THRESHOLD    -500     // sensor value threshold for left-leaning
-#define RIGHT_THRESHOLD    500     // sensor value threshold for right-leaning
+#define VISIBLE_THRESHOLD        -1000    // sensor value threshold for tape visibility
+#define LEFT_THRESHOLD           -500     // sensor value threshold for left-leaning
+#define RIGHT_THRESHOLD          500      // sensor value threshold for right-leaning
 
-#define LOST_ITERATION_THRESHOLD 10  // recovery algorithm phase 0 iteration threshold
-
-static const uint8_t motor_pins[] = {A0, A1, A2, A3, A4, A5}; //Motor driver interfaced via analog pins
+#define LOST_ITERATION_THRESHOLD 10       // recovery algorithm phase 0 iteration threshold
 
 QTRSensors qtr;
 uint16_t sensorValues[NUM_SENSORS];
+
+Adafruit_MotorShield shield;
+Adafruit_DCMotor *motorLeft;
+Adafruit_DCMotor *motorRight;
 
 uint16_t lostIteration;
 uint16_t recoveryPhase;
 
 void setup()
 {
-  //Clearing state of motor driver
-  for (int i = 0; i < 6; i++)
-  {
-    pinMode(motor_pins[i], OUTPUT);
-    digitalWrite(motor_pins[i], LOW);
-  }
-
-  //Setting motor speed
-  analogWrite(MOTOR_PIN_LEFT_SPEED, MOTOR_SPEED - MOTOR_SPEED_COMP_LEFT);
-  analogWrite(MOTOR_PIN_RIGHT_SPEED, MOTOR_SPEED);
-
-  qtr.setTypeRC();
-  // Sensors 0 through 5 are connected to digital pins 2 through 6, respectively
-  qtr.setSensorPins((const uint8_t[]){2, 3, 4, 5, 6}, NUM_SENSORS);
+  qtr.setTypeAnalog();
+  // sensors 0 through 5 are connected to analog inputs 0 through 5, respectively
+  qtr.setSensorPins((const uint8_t[]){0, 1, 2, 3, 4, 5}, NUM_SENSORS);
   qtr.setEmitterPin(EMITTER_PIN);
 
   delay(500);
@@ -59,47 +47,62 @@ void setup()
 
   // TODO: use NeoPixel to indicate end of calibration, then turn off
 
+  shield = Adafruit_MotorShield();
+
+  motorLeft = shield.getMotor(MOTOR_PIN_LEFT);
+  motorRight = shield.getMotor(MOTOR_PIN_RIGHT);
+
+  shield.begin();
+
+  motorLeft->setSpeed(MOTOR_SPEED);
+  motorRight->setSpeed(MOTOR_SPEED);
+
   lostIteration = 0;
   recoveryPhase = 0;
-}
-
-//Reset state of BOTH motors
-void motor_state_reset()
-{
-  for (int i = 0; i < 4; i++)
-  {
-    digitalWrite(motor_pins[i], LOW);
-  }
 }
 
 // Forward on BOTH motors
 void motor_both_forward()
 {
-  motor_state_reset();
-  digitalWrite(MOTOR_PIN_RIGHT_FORWARD, HIGH);
-  digitalWrite(MOTOR_PIN_LEFT_FORWARD, HIGH);
+  motorLeft->run(FORWARD);
+  motorRight->run(FORWARD);
+
+  delay(MOTOR_DURATION);
+
+  motorLeft->run(RELEASE);
+  motorRight->run(RELEASE);
 }
 
 // Reverse on BOTH motors
 void motor_both_reverse()
 {
-  motor_state_reset();
-  digitalWrite(MOTOR_PIN_RIGHT_REVERSE, HIGH);
-  digitalWrite(MOTOR_PIN_LEFT_REVERSE, HIGH);
+  motorLeft->run(BACKWARD);
+  motorRight->run(BACKWARD);
+
+  delay(MOTOR_DURATION);
+
+  motorLeft->run(RELEASE);
+  motorRight->run(RELEASE);
 }
 
 // Forward on LEFT motor only
 void motor_left_forward()
 {
-  motor_state_reset();
-  digitalWrite(MOTOR_PIN_LEFT_FORWARD, HIGH);
+  motorLeft->run(FORWARD);
+
+  delay(MOTOR_DURATION);
+
+  motorLeft->run(RELEASE);
 }
 
 // Forward on RIGHT motor only
 void motor_right_forward()
 {
-  motor_state_reset();
-  digitalWrite(MOTOR_PIN_RIGHT_FORWARD, HIGH);
+  motorRight->run(FORWARD);
+
+  delay(MOTOR_DURATION);
+
+  motorRight->run(RELEASE);
 }
 
 void recovery_alg_reset()
